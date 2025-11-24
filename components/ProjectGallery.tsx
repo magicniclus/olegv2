7331321deffd84c873708d9cf23255c32b5000f9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-11-24 15:24:46 +0100

    Fix gallery 'Show more' button: use galleryImages.length instead of images.length to properly display all images

--- components/ProjectGallery.tsx
+++ components/ProjectGallery.tsx
@@ -63,7 +63,7 @@ export default function ProjectGallery({
   };
 
   const showMoreImages = () => {
-    setDisplayCount(images.length);
+    setDisplayCount(galleryImages.length);
   };
 
   useEffect(() => {
@@ -142,7 +142,7 @@ export default function ProjectGallery({
                 className="bg-orange-500 hover:bg-orange-600 text-white shadow-lg hover:shadow-xl transition-all duration-300"
                 onClick={showMoreImages}
               >
-                Voir plus de réalisations ({images.length - displayCount} restantes)
+                Voir plus de réalisations ({galleryImages.length - displayCount} restantes)
               </Button>
             </motion.div>
           )}
